cffdf6d03c8e2d04705309d824b8625f923f64ad
i160252 <60895680+i160252@users.noreply.github.com>
2020-04-09 23:21:49 +0500

    Add files via upload

---
+++ Hammad.js
@@ -0,0 +1,21 @@
+import React from 'react';
+function Hammad() {
+  return (
+    
+    <div className="App">
+    
+      <h1>
+      Hi there! 
+      </h1>
+      <h2> My name is Hammad </h2>
+        <p>
+          My roll number is i160252
+        </p>
+        <img src={require('./giphy.gif')} />
+       
+     
+    </div>
+  );
+}
+
+export default Hammad;
